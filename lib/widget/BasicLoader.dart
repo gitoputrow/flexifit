e3e2b1ff14dc9148bbf62c645e76850e90443bd7
Gito Putro Wardana <gitoputrowardana@gmail.com>
2023-10-02 17:00:25 +0700

    update

--- lib/widget/BasicLoader.dart
+++ lib/widget/BasicLoader.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
 import 'package:flutter/material.dart';
 import 'package:progress_indicators/progress_indicators.dart';
 
